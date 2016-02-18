56733649d8922172f1d6cdb8dc400718a04c0bda
breezycool <lachlankermode@live.com>
2016-02-18 09:01:30 +0100

    changed testing to tape & enzyme

---
+++ test/components/Title.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import test from 'tape'
+import { shallow } from 'enzyme'
+import Title from '../../src/components/Title';
+
+test('<Title /> component', t => {
+  t.plan(1)
+  const text = 'Welcome to My World'
+  const title = shallow(<Title title={text} />)
+  t.equal(text, title.find('h3').text(), 'renders title prop in h3')
+})
---
+++ test/reducers/fake-reducer.test.js
@@ -0,0 +1,7 @@
+import test from 'tape';
+
+test('practice test', t => {
+  t.plan(1)
+
+  t.equal(1, 1, '1 and 1 are the same')
+})
